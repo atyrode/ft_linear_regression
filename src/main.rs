09c5622c195e150fe7fc8a844a773fcc10a7ca39
atyrode <alex.tyrode@outlook.fr>
2024-03-19 15:00:49 +0100

    add ndarray and ndarray-linalg crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate ndarray;
+use ndarray::Array;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Array::from_vec(vec![1, 2, 3, 4]);
+    println!("Array: {a:?}");
+    let sum = a.sum();
+    println!("Sum of elements: {sum}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ndarray_working() {
+        let a = Array::from_vec(vec![1, 2, 3, 4]);
+        assert_eq!(a.sum(), 10);
+    }
 }
